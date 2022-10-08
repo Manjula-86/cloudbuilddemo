13dd87709d3585c6c093f06fe23dbb67a2a3b406
Manjula Kollu <manjula.kollu@gcp.pwc.com>
2022-10-09 02:08:34 +0530

    added table

--- src/main/java/com/example/CreateDataset.java
+++ src/main/java/com/example/CreateDataset.java
@@ -22,29 +22,67 @@ import com.google.cloud.bigquery.BigQueryException;
 import com.google.cloud.bigquery.BigQueryOptions;
 import com.google.cloud.bigquery.Dataset;
 import com.google.cloud.bigquery.DatasetInfo;
+import com.google.cloud.bigquery.Field;
+import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.StandardSQLTypeName;
+import com.google.cloud.bigquery.StandardTableDefinition;
+import com.google.cloud.bigquery.TableDefinition;
+import com.google.cloud.bigquery.TableId;
+import com.google.cloud.bigquery.TableInfo;
 
 public class CreateDataset {
 
   public static void main(String[] args) {
-    String datasetName = "MY_DATASET_NAME";
+    String datasetName = "dataflow_demo_dataset";
     String location = "US";
     createDataset(datasetName,location);
-  }
-  public static Dataset createDataset(String datasetName, String location) {
-    try {
-      // Initialize client that will be used to send requests. This client only needs to be created
-      // once, and can be reused for multiple requests.
-      BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
-      DatasetInfo datasetInfo = DatasetInfo.newBuilder(datasetName).setLocation(location).build();
-      Dataset newDataset = bigquery.create(datasetInfo);
-      String newDatasetName = newDataset.getDatasetId().getDataset();
-      System.out.println(newDatasetName + " created successfully");
-      return newDatasetName;
-    } catch (BigQueryException e) {
-      System.out.println("Dataset was not created. \n" + e.toString());
-    }
+    String tableName = "Customer";
+    Schema schema =
+		        Schema.of(
+		            Field.of("customername", StandardSQLTypeName.STRING),
+		            Field.of("customerid", StandardSQLTypeName.NUMERIC),
+		            Field.of("isactive", StandardSQLTypeName.BOOL));
+		createTable(datasetName, tableName, schema);
   }
 
-  
+  public static String createDataset(String datasetName, String location) {
+	    	System.out.println("Calling createDataset method....");
+			String newDatasetName=null;
+	    try {
+	      // Initialize client that will be used to send requests. This client only needs to be created
+	      // once, and can be reused for multiple requests.
+	      BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+	      DatasetInfo datasetInfo = DatasetInfo.newBuilder(datasetName).setLocation(location).build();
+	      Dataset newDataset = bigquery.create(datasetInfo);
+	      newDatasetName = newDataset.getDatasetId().getDataset();
+	      System.out.println(newDatasetName + " created successfully");
+	      return newDatasetName;
+	    } catch (BigQueryException e) {
+	      System.out.println("Dataset was not created. \n" + e.toString());
+	    }
+	    System.out.println("End createDataset method....");
+	    return newDatasetName;
+	   
+	}
+	    
+  public static void createTable(String datasetName, String tableName, Schema schema) {
+      System.out.println("Calling createTable method....");
+      try {
+          // Initialize client that will be used to send requests. This client only needs to be created
+          // once, and can be reused for multiple requests.
+          BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
+
+          TableId tableId = TableId.of(datasetName, tableName);
+          TableDefinition tableDefinition = StandardTableDefinition.of(schema);
+          TableInfo tableInfo = TableInfo.newBuilder(tableId, tableDefinition).build();
+
+          bigquery.create(tableInfo);
+          System.out.println("Table created successfully");
+        } catch (BigQueryException e) {
+          System.out.println("Table was not created. \n" + e.toString());
+        }
+      System.out.println("End createTable method....");
+    }
+    
 }
 // [END bigquery_create_dataset]
